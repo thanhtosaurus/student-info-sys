82b59c96e7f55b8cd7a9fb88aa89ccda14153e08
Ryan Pham <ryanpham@Ryans-MacBook-Air-2.local>
2025-04-02 22:15:51 -0700

    Changed port to :5001 for mac compatability

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_URL = 'http://localhost:5000/api';
+export const API_URL = 'http://localhost:5001/api';
 
 // Supabase Configuration
 export const SUPABASE_URL = 'https://asckwevrpijncmxixicc.supabase.co';
